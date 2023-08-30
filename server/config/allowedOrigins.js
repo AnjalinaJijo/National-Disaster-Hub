f54304a9152a621c450e4737ea0d2d615d3c72bb
Anjalina Jijo <75922533+AnjalinaJijo@users.noreply.github.com>
2023-08-30 08:53:56 -0700

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,13 +1,5 @@
 const allowedOrigins=[
-    'http://localhost:3500',
-    'http://localhost:3000',
-    'http://localhost:3500/partner',
-    'http://localhost:3000/Login.js',
-    'http://localhost:3000/Login',
-    'http://localhost:3500/auth',
-    'http://localhost:3500/checkin',
-    'http://localhost:3500/find',
-    'http://localhost:3500/weatherapi'
+''
 ];
 
 module.exports=allowedOrigins;
