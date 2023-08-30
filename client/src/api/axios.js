c22452a91ed37bee767487f3a2a75b6dbc269187
Anjalina Jijo <75922533+AnjalinaJijo@users.noreply.github.com>
2023-08-30 14:11:43 -0700

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL:'http://localhost:3500'
-    baseURL:'https://national-disaster-hub-bont.vercel.app'
+    baseURL:'https://national-disaster-hub-api.vercel.app'
 });
