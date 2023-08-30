a143849fba5c94dd71bf931d5c33ab8c1ad1802b
Anjalina Jijo <75922533+AnjalinaJijo@users.noreply.github.com>
2023-08-30 14:15:45 -0700

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins=[
 'https://national-disaster-hub-bont.vercel.app/weatherapi',
 'https://national-disaster-hub-bont.vercel.app/weatherapi',
 'https://national-disaster-hub-bont.vercel.app',
-'https://national-disaster-hub.onrender.com'
+'https://national-disaster-hub.onrender.com',
+'https://national-disaster-hub-mt2z.vercel.app'
 ];
 
 module.exports=allowedOrigins;
