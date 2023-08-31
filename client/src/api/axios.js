3d857b00d3a986a0427807a5b8d98e6cb4101f70
Anjalina Jijo <75922533+AnjalinaJijo@users.noreply.github.com>
2023-08-30 20:21:39 -0700

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL:'http://localhost:3500'
-    baseURL:'https://national-disaster-hub-4mbykxuk8-anjalinajijo.vercel.app'
+    baseURL:'https://ndh.onrender.com'
 });
